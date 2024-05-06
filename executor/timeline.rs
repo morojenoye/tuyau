00f0ae44dc00ef2b4e46da90edb381487e0a29cc
mekosko <mekosko@morojenoye.com>
2024-05-06 20:10:09 +0000

    Implement sketch

--- executor/timeline.rs
+++ executor/timeline.rs
@@ -1,5 +1,17 @@
-pub trait Query {}
+use ruma::{events::pdu::RoomV3Pdu, EventId};
+
+use crate::{MaybePdu, MyResult};
+
+pub trait Query {
+	async fn append(&self, pdu_id: &EventId, pdu: &RoomV3Pdu) -> MyResult<()>;
+	async fn select(&self, pdu_id: &EventId) -> MyResult<MaybePdu>;
+}
 
 pub struct Executor<'a, T: Query> {
 	inner: &'a T,
+}
+
+impl<'a, T: Query> Executor<'a, T> {
+	pub async fn append(&self, pdu_id: &EventId, pdu: &RoomV3Pdu) {}
+	pub async fn select(&self, pdu_id: &EventId) {}
 }
--- main.rs
+++ main.rs
@@ -1,6 +1,12 @@
+use ruma::events::pdu::RoomV3Pdu;
+
 mod confconf;
 mod executor;
 mod models;
 mod router;
 
-fn main() {}
+pub type MyResult<T> = anyhow::Result<T>;
+pub type MaybePdu = Option<RoomV3Pdu>;
+
+#[tokio::main]
+async fn main() {}
