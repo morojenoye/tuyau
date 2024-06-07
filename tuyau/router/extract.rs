96f6fb4657207aa0130dda4ebdb785aebabd65f8
mekosko <mekosko@morojenoye.com>
2024-06-07 13:02:18 +0000

    Sketch ruma extractor

--- tuyau/router/extract.rs
+++ tuyau/router/extract.rs
@@ -19,6 +19,7 @@ use ruma::{
 	},
 	serde::Base64,
 	server_util::authorization::XMatrix,
+	signatures::verify_json,
 	CanonicalJsonValue, OwnedServerName, OwnedServerSigningKeyId,
 };
 
@@ -58,10 +59,14 @@ where
 	type Rejection = Reply;
 
 	async fn from_request(req: Request<Body>, ctx: &Executor<'a, T>) -> Result<Self> {
+		// =================================================================
+
 		let AuthScheme::ServerSignatures = R::METADATA.authentication else {
 			return Err(make_unauthorized_error());
 		};
 		let (mut parts, body) = req.with_limited_body().into_parts();
+
+		// =================================================================
 
 		let header = match parts.extract::<TypedHeader<AuthHeader>>().await {
 			Ok(TypedHeader(Authorization(header))) => Ok(header),
@@ -75,12 +80,16 @@ where
 			Some(response) => return Err(response),
 			None => (),
 		}
-		let mut request_map = BTreeMap::new();
 
-		let keys = ["method", "uri", "destination", "origin"];
+		// =================================================================
+
+		let keys: [&str; 4] = ["method", "uri", "destination", "origin"];
+		let mut request_map = BTreeMap::new();
 
 		request_map.insert(keys[0].to_string(), parts.method.to_string().into());
 		request_map.insert(keys[1].to_string(), parts.uri.to_string().into());
+
+		// =================================================================
 
 		let body = body::to_bytes(body, usize::MAX).await;
 		let body = body.map_err(|_| make_internal_server_error())?;
@@ -89,23 +98,31 @@ where
 			Path::<PathArgs>::from_request_parts(&mut parts, &()).await.unwrap(),
 			serde_json::from_slice::<CanonicalJsonValue>(&body).ok(),
 		);
-		value.map(|v| request_map.insert("content".to_string(), v));
+		value.map(|value| request_map.insert("content".to_string(), value));
 
-		let signatures = BTreeMap::from([(
+		// =================================================================
+
+		let signatures: BTreeMap<String, CanonicalJsonValue> = BTreeMap::from([(
 			header.key.to_string(),
 			CanonicalJsonValue::String(header.sig),
 		)]);
-		let signatures = CanonicalJsonValue::Object(BTreeMap::from([(
+		let signatures: BTreeMap<String, CanonicalJsonValue> = BTreeMap::from([(
 			header.origin.to_string(),
 			CanonicalJsonValue::Object(signatures),
-		)]));
+		)]);
+		let signatures: _ = CanonicalJsonValue::Object(signatures);
+
 		request_map.insert("signatures".to_string(), signatures);
+
+		// =================================================================
 
 		let server_name = ctx.server_name.to_string().into();
 		let origin = header.origin.to_string().into();
 
 		request_map.insert(keys[2].to_string(), server_name);
 		request_map.insert(keys[3].to_string(), origin);
+
+		// =================================================================
 
 		let (server_keys, mut p_keys_map) = (
 			ctx.keyserver.get_server_keys(&header.origin).await,
@@ -117,6 +134,11 @@ where
 		let v: BTreeMap<String, Base64> = iter.map(fmt).collect();
 		p_keys_map.insert(header.origin.to_string(), v);
 
+		// =================================================================
+
+		if let Err(_) = verify_json(&p_keys_map, &request_map) {
+			return Err(make_internal_server_error());
+		}
 		let http_request = http::Request::from_parts(parts, body);
 
 		let body = R::try_from_http_request(http_request, &path_args).unwrap();
